80311ebf2892692a38fed0d201e4269ac92391e1
Chang Metellus <metellus.chang@gamil.com>
2022-12-21 04:41:11 -0500

    test file employee done

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,11 +1,48 @@
-const { describe } = require("yargs");
 const Employee = require("../lib/Employee");
 
-describe("Employee class", () => {
-  describe("getName, getId, getEmail, getRole method", () => {
-    const employee = new Employee();
+describe("Instantiate", () => {
+  const testEmployee = new Employee();
+  expect(typeof(testEmployee)).toBe("object");
+});
 
-    expect(employee).toEqual({name: "Eva", id: 001, email: "eva100@gamil.com", role: "Employee" })
-  })
-}
-)
+describe("Name", () => {
+  const name = "Chang";
+  const test = new Employee(name);
+  expect(typeof(test.name)).toBe(name);
+});
+
+describe("Id", () => {
+  const id = 1996;
+  const test = new Employee("Chang", id);
+  expect(typeof(test.id)).toBe(id);
+});
+
+describe("Email", () => {
+  const email = "test@gmail.com";
+  const test = new Employee("Chang", 1, email);
+  expect(typeof(test.email)).toBe(email);
+});
+
+describe("getName Method", () => {
+  const name = "Chang";
+  const test = new Employee(name);
+  expect(typeof(test.getName())).toBe(name);
+});
+
+describe("getId Method", () => {
+  const id = 1996;
+  const test = new Employee(id);
+  expect(typeof(test.getId())).toBe(id);
+});
+
+describe("getEmail Method", () => {
+  const email = "test@gmail.com";
+  const test = new Employee(email);
+  expect(typeof(test.getEmail())).toBe(email);
+});
+
+describe("getRole Method", () => {
+  const role = "Employee";
+  const test = new Employee(role);
+  expect(typeof(test.getRole())).toBe(role);
+});
